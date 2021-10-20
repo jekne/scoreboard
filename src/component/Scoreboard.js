bd2462d1877fa65ec4d6d6f5884ee43c91f06c41
Johann Eugen <jekneto@hotmail.com>
2021-10-20 15:12:31 +0200

    This we have sorted by name, without the score

--- src/component/Scoreboard.js
+++ src/component/Scoreboard.js
@@ -48,12 +48,49 @@
 
 //we are add sortin to scoreboar, adding sorting logic and called the score at the screen
 
+// import { useState } from "react";
+// import AddPlayerForm from "./AddPlayerForm";
+// import Player from "./Players";
+
+// function compare_score(player_a, player_b) {
+//   return player_b.score - player_a.score;
+// }
+
+// export default function Scoreboard() {
+//   const [players, set_players] = useState([
+//     { id: 1, name: "Violeta", score: 11 },
+//     { id: 2, name: "Eszter", score: 14 },
+//     { id: 3, name: "Jeroen v2", score: 4 },
+//     { id: 4, name: "Lisa", score: 42 },
+//   ]);
+//   const players_sorted = [
+//     { id: 1, name: "Violeta", score: 11 },
+//     { id: 2, name: "Eszter", score: 14 },
+//     { id: 3, name: "Jeroen v2", score: 4 },
+//     { id: 4, name: "Lisa", score: 42 },
+//   ].sort(compare_score);
+
+//   return (
+//     <div className="Scoreboard">
+//       <p>Player's scores:</p>
+//       <ul>
+//         {players_sorted.map((player) => (
+//           <Player key={player.id} name={player.name} score={player.score} />
+//         ))}
+//       </ul>
+//       <AddPlayerForm />
+//     </div>
+//   );
+// }
+
+// in this part we are gonna sort by the name (remeber i remove the player by score, in the next exercise i am gonna ned both)
+
 import { useState } from "react";
 import AddPlayerForm from "./AddPlayerForm";
 import Player from "./Players";
 
-function compare_score(player_a, player_b) {
-  return player_b.score - player_a.score;
+function compare_Name(player_a, player_b) {
+  return player_a.name.localeCompare(player_b.name);
 }
 
 export default function Scoreboard() {
@@ -63,12 +100,7 @@ export default function Scoreboard() {
     { id: 3, name: "Jeroen v2", score: 4 },
     { id: 4, name: "Lisa", score: 42 },
   ]);
-  const players_sorted = [
-    { id: 1, name: "Violeta", score: 11 },
-    { id: 2, name: "Eszter", score: 14 },
-    { id: 3, name: "Jeroen v2", score: 4 },
-    { id: 4, name: "Lisa", score: 42 },
-  ].sort(compare_score);
+  const players_sorted = [...players].sort(compare_Name);
 
   return (
     <div className="Scoreboard">
@@ -82,3 +114,5 @@ export default function Scoreboard() {
     </div>
   );
 }
+
+// the exercise bellow add a new state variable to the Scoreboard
